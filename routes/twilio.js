56764b95386df2859b1a721318e049cccae04331
stinethebean <matheneyc@gmail.com>
2014-05-03 16:58:10 -0700

    pulling matts changes to server.js where things actually are

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,48 +1,136 @@
-
-
-var twilio_credentials = require('../twilio_credentials.json');
-
-var twilio = require('twilio');
-var client = new twilio.RestClient(twilio_credentials.twilio_sid, twilio_credentials.twilio_auth);
-
-var messages = [];
-
-exports.receive_message = function(req, res){
-
-	messages.push(req.query);
-	console.log(messages);
-
-	res.send('');
-};
-
-exports.retrieve_messages = function(req, res){
-	return res.json(messages);
-};
-
-
-
-/*
- * GET home page.
+/**
+ * Module dependencies.
  */
 
-exports.index = function(req, res){
 
-	client.sms.messages.list(function(error, data){
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var twilio_credentials = require('./twilio_credentials.json');
+var twilio = require('twilio');
 
-		if(!error){
-			data.messages.forEach(function(message) {
-				console.log(message.Body);
-			});
-		}
 
-		res.render('index', {});
+var client = new twilio.RestClient(twilio_credentials.twilio_sid, twilio_credentials.twilio_auth);
 
-	});
+
+
+
+// { ToCountry: 'US',
+//     ToState: 'VA',
+//     SmsMessageSid: 'SMdbef8677f992a26b28df2bc4305035a9',
+//     NumMedia: '0',
+//     ToCity: 'VIENNA',
+//     FromZip: '94546',
+//     SmsSid: 'SMdbef8677f992a26b28df2bc4305035a9',
+//     FromState: 'CA',
+//     SmsStatus: 'received',
+//     FromCity: 'HAYWARD',
+//     Body: 'M: Svc\nT: SF \nW: LAS\nTh: home',
+//     FromCountry: 'US',
+//     To: '+17037634332',
+//     ToZip: '22182',
+//     MessageSid: 'SMdbef8677f992a26b28df2bc4305035a9',
+//     AccountSid: 'AC50cf5db22487012e70e1eb2e4335bcba',
+//     From: '+15107541837',
+//     ApiVersion: '2010-04-01' }
+
+
+
+
+var location = {};
+
+
+var receive_message = function(req, res){
+
+
+
+
+  var message = req.query;
+
+
+  console.log(message);
+
+
+  var messageArray = message.Body.split(':');
+
+
+  if(messageArray.length !== 3){
+    return sendSms(message.From, "Invalid message", function(){ res.send('') } );
+  }
+
+
+  switch(messageArray[0]){
+    case 's':
+
+
+
+
+      break;
+    case 'r':
+
+
+      break;
+    default:
+      return sendSms(message.From, "Invalid command", function(){ res.send('') } );
+  }
+
+
+  var sms = new Sms({
+    number: String,
+    time: Date,
+    location: String
+  });
+
+
+
+
+
 
 };
 
 
+var sendSms = function(number, message, cb){
+  console.log("sending "+message+"to number: " + number);
+
+
+  returnclient.sendMessage({
+    to:number,
+    from: '+17037634332',
+    body: message
+  }, cb)
+}
 
 
 
 
+var app = express();
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+
+app.get('/receive_message', receive_message);
+
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
